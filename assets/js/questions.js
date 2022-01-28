ba225016b4eba5fa427b43b32c55845f1b8b2b54
Miguel Mulet <mmiguel032202@gmail.com>
2022-01-28 18:38:48 -0500

    Creation of JS Questions

---
+++ assets/js/questions.js
@@ -0,0 +1,57 @@
+var questions = [
+    {
+      title: "Is JavaScript a case-sensitive language?",
+      choices: ["True", "False"],
+      answer: "True"
+    },
+    {
+      title: "Which of the following is the correct syntax to redirect a url using JavaScript?",
+      choices: ["document.location='http://www.newlocation.com';", "browser.location='http://www.newlocation.com';", "navigator.location='http://www.newlocation.com';", "window.location='http://www.newlocation.com';"],
+      answer: "window.location='http://www.newlocation.com';"
+    },
+    {
+      title: "Which of the following type of variable is visible only within a function where it is defined?",
+      choices: ["global variable", "local variable", "Both of the above.", "None of the above."],
+      answer: "local variable"
+    },
+    {
+      title: "All user-defined objects and built-in objects are descendants of an object called Object?",
+      choices: ["True", "False"],
+      answer: "True"
+    },
+    {
+      title: "Which of the following function of Number object defines how many total digits to display of a number?",
+      choices: ["toExponential()", "toFixed()", "toLocaleString()", "toPrecision()"],
+      answer: "toPrecision()"
+    },
+    {
+      title: "Which of the following function of String object returns the index within the calling String object of the first occurrence of the specified value?",
+      choices: ["substr()", "search()", "lastIndexOf()", "indexOf()"],
+      answer: "indexOf()"
+    },
+    {
+      title: "Which of the following function of String object returns a string representing the specified object?",
+      choices: ["toLocaleUpperCase()", "toUpperCase()", "toString()", "substring()"],
+      answer: "toString()"
+    },
+    {
+      title: "Which of the following function of String object causes a string to be displayed as a superscript, as if it were in a <sup> tag?",
+      choices: ["sup()", "small()", "strike()", "sub()"],
+      answer: "sup()"
+    },
+    {
+      title: "Which of the following function of Array object calls a function for each element in the array?",
+      choices: ["concat()", "every()", "filter()", "forEach()"],
+      answer: "forEach()"
+    },
+    {
+      title: "Which of the following function of Array object applies a function simultaneously against two values of the array (from right-to-left) as to reduce it to a single value?",
+      choices: ["pop()", "push()", "reduce()", "reduceRight()"],
+      answer: "reduceRight()"
+    },
+    {
+      title: "When interpreter encounters an empty statements, what it will do:",
+      choices: ["Shows a warning", "Prompts to complete the statement", "Throws an error", "Ignores the statements"],
+      answer: "Ignores the statements"
+    }
+];
